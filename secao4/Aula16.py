c9e749c3c3e16c06f47b1cd469f5e966d9cbb75e
Guilherme Painko Scalcon <gui.painko2393@gmail.com>
2024-02-21 13:59:26 -0300

    secao 4

---
+++ secao4/Aula16.py
@@ -0,0 +1,26 @@
+#json.dump e json.load com arquivos
+
+import json
+import os
+
+NOME_ARQUIVO = 'Aula16.json'
+CAMINHO_ABSOLUTO_ARQUIVO = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        NOME_ARQUIVO
+    )
+)
+
+filme={'title': 'O Senhor Do Anéis: A Sociedade do Anel', 
+        'Original_title': 'The Lord of The Rings:The Fellowship of the Ring',
+        'is_movie': True, 
+        'imdb_rating': 8.8, 
+        'year': 2001, 
+        'characters': ['Frodo', 'Sam', 'Gandald', 'Legolas', 'Boromir'], 'budget': None}
+
+with open(CAMINHO_ABSOLUTO_ARQUIVO,'w')as arquivo:
+    json.dump(filme,arquivo,ensure_ascii=False,indent=2)
+
+with open(CAMINHO_ABSOLUTO_ARQUIVO,'r')as arquivo:
+    filme_do_json=json.load(arquivo)
+    print(filme)
